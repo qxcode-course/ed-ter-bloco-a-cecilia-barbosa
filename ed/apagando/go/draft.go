edb22c4148bda31b82b6c2758a88d954e93c292a
Cecília Barbosa <aceciliasb07@gmail.com>
2026-03-24 14:26:41 +0000

    salvando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,5 +1,22 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	qtd_inicial := 0
+	fmt.Scan(&qtd_inicial)
+
+	id := make([]int, qtd_inicial)
+
+	for i := 0; i < qtd_inicial; i++ {
+		fmt.Scan(&id)
+	}
+
+	left := 0
+	sairam := make([]int, left)
+	fmt.Scan(&left)
+	for i := 0; i < left; i++ {
+		fmt.Scan(&sairam)
+	}
+
 }
