8771a57b94ed848c04f206b10e42ec649aef06de
Cecília Barbosa <aceciliasb07@gmail.com>
2026-03-29 20:03:22 +0000

    rotacoes

--- ed/rotacoes/go/draft.go
+++ ed/rotacoes/go/draft.go
@@ -12,25 +12,26 @@ func main() {
 	for i := 0; i < tamT; i++ {
 		fmt.Scan(&vetor[i])
 	}
-
-	if rot == 0 {
-		fmt.Printf("[")
-		for _, i := range vetor {
-			fmt.Printf(" %v", i)
-		}
-		fmt.Println(" ]")
-	} else {
-		for i := 1; i <= rot; i++ {
-			j := 0
-			vetor[j] = vetor[len(vetor)-1]
-			j++
-
-		}
-
-		fmt.Printf("[")
-		for _, i := range vetor {
-			fmt.Printf(" %v", i)
-		}
-		fmt.Println(" ]")
+	//ajuda da Sophia Muniz
+	rotacao := rot % tamT // usa o resto para se preocupar apenas com as que sobraram,
+	if rotacao < 0 {      //nao as que dao loop completo e ja volta do zero
+		rotacao += tamT
 	}
+	// foi essa lógica que ela me ajudou
+
+	vetFinal := make([]int, 0, tamT) // slice dinamico
+
+	for i := tamT - rotacao; i < tamT; i++ { // coloca os rotacionados no inicio
+		vetFinal = append(vetFinal, vetor[i])
+	}
+	for i := 0; i < tamT-rotacao; i++ { // coloca os primeiros no final
+		vetFinal = append(vetFinal, vetor[i])
+	}
+
+	fmt.Printf("[")
+	for _, i := range vetFinal {
+		fmt.Printf(" %v", i)
+	}
+	fmt.Println(" ]")
+
 }
