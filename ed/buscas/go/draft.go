afbda5227fcab936a6dbca41cbb76d479738eb3c
Cecília Barbosa <aceciliasb07@gmail.com>
2026-03-31 23:17:49 +0000

    buscas(3/3)

--- ed/buscas/go/draft.go
+++ ed/buscas/go/draft.go
@@ -4,26 +4,25 @@ import "fmt"
 
 func main() {
 	nConsultas, nBuscas := 0, 0
-
+    
 	fmt.Scan(&nConsultas)
-	consulta := ""
+	consulta := make([]string, nConsultas)
 	matriz := make(map[string]int)
-
+	
 	for i := 0; i < nConsultas; i++ {
-		fmt.Scanln(&consulta)
-		matriz[consulta]++
+		fmt.Scan(&consulta[i])
+		matriz[consulta[i]]++
 	}
 
 	fmt.Scan(&nBuscas)
-	busca := ""
+	busca := make([]string, nBuscas)
 	result := make([]int, nBuscas)
 
-	for i := 0; i < nBuscas; i++ { 
-		fmt.Scanln(&busca)
-		result[i] = matriz[busca]
+	for i := 0; i < nBuscas; i++ {
+		fmt.Scan(&busca[i])
+		result[i] = matriz[busca[i]]
 	}
 
 	saida_f := fmt.Sprintf("%v", result)
 	fmt.Println(saida_f[1 : len(saida_f)-1])
-
 }
