3b7f03d6686c8fb5a840c9f4a9e8f66ae2c25aa2
Cecília Barbosa <aceciliasb07@gmail.com>
2026-04-03 03:03:45 +0000

    resto e div

--- ed/resto/go/draft.go
+++ ed/resto/go/draft.go
@@ -1,8 +1,20 @@
 package main
+
 import "fmt"
 
-
+func rec(num int) {
+	if num == 0 {
+		return 
+	} else {
+        div := num / 2
+	    resto := num % 2
+	    rec(div)
+        fmt.Printf("%v %v\n", div, resto)
+    }
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	num := 0
+	fmt.Scan(&num)
+	rec(num)
 }
