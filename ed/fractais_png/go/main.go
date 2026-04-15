23de4d3817b56ecd3f3edef72092b14bbd4cf691
Cecília Barbosa <aceciliasb07@gmail.com>
2026-04-15 01:12:56 +0000

    hexagono com circulos

--- ed/fractais_png/go/main.go
+++ ed/fractais_png/go/main.go
@@ -9,19 +9,28 @@ func ri(inf, sup int) float64 {
 	return float64(rand.Intn(sup-inf+1) + inf)
 }
 
-func penta(pen *Pen, dist float64) {
-	if dist < 1{
+func circulos(pen *Pen, dist float64) {
+	if dist < 6 {
 		return
 	}
-	angulo := 72.0
-	fator := 0.3
+	angulo := 60.0
+	fator := 0.339
 
-	for range 5{
-		pen.SetLineWidth(0.5)
+	pen.DrawCircle(dist)
+	for range 6 {
+		pen.SetLineWidth(0.99)
 		pen.Right(angulo)
+
+		pen.Up()
 		pen.Walk(dist)
-		penta(pen, dist*fator)
+		pen.Down()
+
+		pen.DrawCircle(dist * fator)
+		circulos(pen, dist*fator)
+
+		pen.Up()
 		pen.Walk(-dist)
+		pen.Down()
 	}
 }
 
@@ -32,9 +41,9 @@ func main() {
 	pen.FillSquare(800, 800)
 
 	pen.SetPosition(400, 420)
-	pen.SetRGB(100, 215, 233)
-	penta(pen, 280)
+	pen.SetRGB(250, 250, 250)
+	circulos(pen, 280)
 
-	pen.SavePNG("pentagono.png")
+	pen.SavePNG("circulos.png")
 	fmt.Println("PNG file created successfully.")
 }
