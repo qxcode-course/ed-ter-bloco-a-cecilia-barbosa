ba010106a2f1bd73aac0f18968fb99903b295a5c
Cecília Barbosa <aceciliasb07@gmail.com>
2026-05-27 18:39:27 +0000

    multiset (1/2)

--- ed/multiset/src/go/main.go
+++ ed/multiset/src/go/main.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type Multiset struct { //
@@ -22,18 +22,53 @@ func NewMultiSet(capacity int) *Multiset { // novo vetor
 	}
 }
 
-func (v *Multiset) Insert(value int) {//error {
-	if v.size < v.capacity {
-		v.data[v.size] = value
-		v.size++
-	} else {
-		
+// mesmos de vetbuild
+func (v *Multiset) Reserve(capNova int) {
+	if capNova <= v.capacity {
+		return
 	}
-	
+	nData := make([]int, capNova)
+	for i := range v.size {
+		nData[i] = v.data[i]
+	}
+	v.data = nData
+	v.capacity = capNova
+
 }
 
-func (v *Multiset) String() string {
-	return "[" + Join(v.data[0:v.size], ", ") + "]"
+// quase igual de vetbuild
+func (v *Multiset) Insert(value int) {
+	if v.size == v.capacity {
+		capNova := v.capacity * 2
+		if capNova == 0 {
+			capNova = 1
+		}
+		v.Reserve(capNova)
+	}
+
+	v.data[v.size] = value
+	v.size++
+
+	for i := v.size - 1; i > 0 && v.data[i] < v.data[i-1]; i-- {
+		temp := v.data[i]
+		v.data[i] = v.data[i-1]
+		v.data[i-1] = temp
+	}
+}
+
+// mesma de vetbuild
+func (v *Multiset) IndexOf(value int) int {
+	for i := range v.size {
+		if v.data[i] == value {
+			return i
+		}
+	}
+	return -1
+}
+
+// mesma de vetbuild
+func (v *Multiset) Contains(value int) bool {
+	return v.IndexOf(value) != -1
 }
 
 func Join(slice []int, sep string) string {
@@ -45,6 +80,46 @@ func Join(slice []int, sep string) string {
 		result += sep + fmt.Sprintf("%d", value)
 	}
 	return result
+}
+
+// mesmo de vetbuild
+func (v *Multiset) String() string {
+	return "[" + Join(v.data[:v.size], ", ") + "]"
+}
+
+// quase a mesma de vetbuild
+func (v *Multiset) Erase(indice int) {
+	if indice < 0 || indice >= v.size {
+		return
+	}
+
+	for i := indice; i < v.size-1; i++ {
+		v.data[i] = v.data[i+1]
+	}
+
+	v.size--
+}
+
+// mesma de vetbuild
+func (v *Multiset) Clear() {
+	v.size = 0
+}
+
+func (v *Multiset) Count(value int) int {
+	count := 0
+	for i := range v.size {
+		if v.data[i] == value {
+			count++
+		}
+	}
+	return count
+}
+
+func (v *Multiset) Unique () int {
+	if v.size == 0 {
+		return 0
+	}
+	return 0
 }
 
 func main() {
@@ -76,13 +151,26 @@ func main() {
 		case "show":
 			fmt.Println(ms.String())
 		case "erase":
-			//value, _ := strconv.Atoi(args[1])
+			value, _ := strconv.Atoi(args[1])
+			indice := ms.IndexOf(value)
+			if indice == -1 {
+				fmt.Println("value not found")
+			} else {
+				ms.Erase(indice)
+			}
 		case "contains":
-			//value, _ := strconv.Atoi(args[1])
+			value, _ := strconv.Atoi(args[1])
+			if ms.Contains(value) {
+				fmt.Println("true")
+			} else {
+				fmt.Println("false")
+			}
 		case "count":
-			//value, _ := strconv.Atoi(args[1])
+			value, _ := strconv.Atoi(args[1])
+			fmt.Println(ms.Count(value))
 		case "unique":
 		case "clear":
+			ms.Clear()
 		default:
 			fmt.Println("fail: comando invalido")
 		}
