112e9db9db7f480e7bae15710694c609699c566b
Cecília Barbosa <aceciliasb07@gmail.com>
2026-05-31 23:56:11 +0000

    lista

--- ed/lista_d1/src/go/main.go
+++ ed/lista_d1/src/go/main.go
@@ -4,26 +4,114 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Node struct {
-	Value int            // Valor é público
-    next *Node           // o próximo nó da lista
-    prev *Node 		     // nó anterior
+	Value int   // Valor é público
+	next  *Node // o próximo nó da lista
+	prev  *Node // nó anterior
 }
 
-func NewList() *Node {
-	return &Node{
+type LList struct {
+	root *Node
+	size int
+}
 
+func NewLList() *LList {
+	dlist := &LList{}
+	dlist.root = &Node{} // cria o nó
+	dlist.root.next = dlist.root
+	dlist.root.prev = dlist.root
+
+	return dlist
+}
+
+func (l *LList) Size() int {
+	return l.size
+}
+
+func (l *LList) Clear() {
+	l.root.next = l.root
+	l.root.prev = l.root
+	l.size = 0
+}
+
+// inserindo no inicio
+func (l *LList) PushFront(value int) {
+	no := &Node{
+		Value: value,
 	}
+
+	primeiro := l.root.next
+	no.next = primeiro
+	no.prev = l.root
+	l.root.next = no
+	primeiro.prev = no
+
+	l.size++
 }
 
+// adiciona um novo nó com esse valor no fim da lista
+func (l *LList) PushBack(value int) {
+	no := &Node{
+		Value: value,
+	}
 
+	final := l.root.prev
+	no.prev = final
+	no.next = l.root
+	final.next = no
+	l.root.prev = no
+
+	l.size++
+}
+
+// remove o primeiro valor da lista se existir
+func (l *LList) PopFront() {
+	if l.size == 0 {
+		return
+	}
+	primeiro := l.root.next
+	prox := primeiro.next
+	l.root.next = prox
+	prox.prev = l.root
+	l.size--
+}
+
+// remove o último valor da lista se existir
+func (l *LList) PopBack() {
+	if l.size == 0 {
+		return
+	}
+	final := l.root.prev
+	pFinal := final.prev
+	pFinal.next = l.root
+	l.root.prev = pFinal
+	l.size--
+}
+
+// quase a msm de vetbuild
+func (l *LList) String() string {
+	if l.size == 0 {
+		return "[]"
+	}
+	var result strings.Builder
+	no := l.root.next
+	fmt.Fprintf(&result, "%d", no.Value)
+	no = no.next
+
+	for i := 1; i < l.size; i++ {
+		fmt.Fprintf(&result, ", %d", no.Value)
+		no = no.next
+	}
+	return "[" + result.String() + "]"
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
- 	ll := NewLList()
+	ll := NewLList()
 
 	for {
 		fmt.Print("$")
@@ -42,25 +130,25 @@ func main() {
 
 		switch cmd {
 		case "show":
-			// fmt.Println(ll.String())
+			fmt.Println(ll.String())
 		case "size":
-			// fmt.Println(ll.Size())
+			fmt.Println(ll.Size())
 		case "push_back":
-			// for _, v := range args[1:] {
-			// 	num, _ := strconv.Atoi(v)
-			// 	ll.PushBack(num)
-			// }
+			for _, v := range args[1:] {
+				num, _ := strconv.Atoi(v)
+				ll.PushBack(num)
+			}
 		case "push_front":
-			// for _, v := range args[1:] {
-			// 	num, _ := strconv.Atoi(v)
-			// 	ll.PushFront(num)
-			// }
+			for _, v := range args[1:] {
+				num, _ := strconv.Atoi(v)
+				ll.PushFront(num)
+			}
 		case "pop_back":
-			// ll.PopBack()
+			ll.PopBack()
 		case "pop_front":
-			// ll.PopFront()
+			ll.PopFront()
 		case "clear":
-			// ll.Clear()
+			ll.Clear()
 		case "end":
 			return
 		default:
