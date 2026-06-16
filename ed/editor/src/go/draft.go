7a8dc1935b5e001892615ec1484d32f43951b650
Cecília Barbosa <aceciliasb07@gmail.com>
2026-06-16 14:16:55 +0000

    16/06

--- ed/editor/src/go/draft.go
+++ ed/editor/src/go/draft.go
@@ -2,5 +2,37 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    entrada := ""
+    fmt.Scanln(&entrada)
+
+    texto := []rune{}
+    posicao := 0
+
+    for c := range entrada {
+        switch c {
+        case '>':
+            if posicao < len(texto){
+                posicao++
+            }
+        case '<':
+            if posicao > 0 {
+                posicao--
+            }
+        case 'B':
+            if posicao < len(texto){
+                texto = append(texto[:posicao-1], texto[posicao:]...)
+                posicao--
+            }
+        case 'D':
+            if posicao < len(texto){
+                texto = append(texto[:posicao], texto[posicao+1:]...)
+            }
+        case 'R':
+        default:
+        }
+    }
+
+    result := string(texto[:posicao]) + "|" + string(texto[posicao:])
+    fmt.Println(result)
+
 }
