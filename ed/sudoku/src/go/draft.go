192736f0c257ec69ad9f488b25d8db52f4bbe7f9
Cecília Barbosa <aceciliasb07@gmail.com>
2026-06-23 13:33:29 +0000

    23/03_pt1

--- ed/sudoku/src/go/draft.go
+++ ed/sudoku/src/go/draft.go
@@ -1,5 +1,16 @@
 package main
 import "fmt"
+
+func sudoku(x int, y int) bool {
+    if coord[y][x] != 0 {         
+
+    } else{
+
+    }
+
+    return false
+}
+
 func main() {
     fmt.Println("Hello, World!")
 }
