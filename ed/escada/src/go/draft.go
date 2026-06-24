c0427fa6a451ae689124ff062c37ca69fb6e89b9
Cecília Barbosa <aceciliasb07@gmail.com>
2026-06-24 13:55:51 +0000

    escada / recursao

--- ed/escada/src/go/draft.go
+++ ed/escada/src/go/draft.go
@@ -1,5 +1,30 @@
 package main
+
 import "fmt"
+
+func escada(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	a := 1
+	b := 1
+	c := 2
+
+	atual := 0
+
+	for i := 4; i <= n; i++ {
+		atual = c + a
+		a = b
+		b = c
+		c = atual
+	}
+	return c
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	n := 0
+	fmt.Scan(&n)
+
+    fmt.Printf("%d\n", escada(n))
 }
