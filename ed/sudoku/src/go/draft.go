c5cbe979dcbbd5c9d2394b4e483516f51db03f65
Cecília Barbosa <aceciliasb07@gmail.com>
2026-06-29 05:08:57 +0000

    na luta

--- ed/sudoku/src/go/draft.go
+++ ed/sudoku/src/go/draft.go
@@ -1,16 +1,65 @@
 package main
+
 import "fmt"
 
-func sudoku(x int, y int) bool {
-    if coord[y][x] != 0 {         
+func sudoku(matriz [][]rune, l int, c int) bool {
+	if noLimite(matriz, l, c) == false || matriz[l][c] != '.' {
+		return false
+	}
 
-    } else{
+	N := len(matriz)
 
-    }
+	if l == N-1 && c == N-1 {
+		matriz[l][c] = 'X'
+		return true
+	}
 
-    return false
+	matriz[l][c] = 'X'
+
+	if sudoku(matriz, l, c+1) {
+		return true
+	}
+
+	if sudoku(matriz, l+1, c) {
+		return true
+	}
+
+	if sudoku(matriz, l, c-1) {
+		return true
+	}
+
+	if sudoku(matriz, l-1, c) {
+		return true
+	}
+
+	matriz[l][c] = '.'
+
+	return false
+}
+
+func noLimite(matriz [][]rune, l, c int) bool {
+	N := len(matriz)
+	return l >= 0 && l < N && c >= 0 && c < N
+}
+
+func podeCaminhar(matriz [][]rune, l, c int) bool {
+	return noLimite(matriz, l, c) && matriz[l][c] == '.'
 }
 
 func main() {
-    fmt.Println("Hello, World!")
+	N := 0
+	fmt.Scan(&N)
+
+	matriz := make([][]rune, N)
+	for i := range matriz {
+		linha := ""
+		fmt.Scan(&linha)
+		matriz[i] = []rune(linha)
+	}
+
+	if sudoku(matriz, 0, 0) {
+		for _, linha := range matriz {
+			fmt.Println(string(linha))
+		}
+	}
 }
