25e6aed526e43b78fb3cbbac145f3f954525a400
Cecília Barbosa <aceciliasb07@gmail.com>
2026-07-03 05:14:50 +0000

    balanceada

--- ed/balanceada/go/draft.go
+++ ed/balanceada/go/draft.go
@@ -1,5 +1,33 @@
 package main
+
 import "fmt"
+
+func balanceada(entrada string) string {
+	pilha := []rune{}
+	for _, temp := range entrada {
+		if temp == '(' || temp == '[' {
+			pilha = append(pilha, temp)
+		} else if temp == ')' || temp == ']' {
+			if len(pilha) == 0 {
+				return "nao balanceado"
+			}
+			cima := pilha[len(pilha)-1]
+			pilha = pilha[:len(pilha)-1]
+
+			if (temp == ')' && cima != '(') || (temp == ']' && cima != '[') {
+				return "nao balanceado"
+			}
+		}
+	}
+	if len(pilha) == 0 {
+		return "balanceado"
+	}
+	return "nao balanceado"
+}
+
 func main() {
-    fmt.Println("qxcode")
+	entrada := ""
+
+	fmt.Scan(&entrada)
+	fmt.Println(balanceada(entrada))
 }
