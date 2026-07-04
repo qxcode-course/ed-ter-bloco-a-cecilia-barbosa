e5b650f82237c902c7120664d3e8dfb945335fcf
Cecília Barbosa <aceciliasb07@gmail.com>
2026-07-04 05:22:18 +0000

    subset_num

--- ed/pilhado/src/go/main.go
+++ ed/pilhado/src/go/main.go
@@ -6,3 +6,80 @@ import (
 	"os"
 )
 
+type Pos struct {
+	l, c int
+}
+
+func main() {
+	nl, nc := 0, 0
+	fmt.Scan(&nl, &nc)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	grid := make([][]rune, nl)
+	var start, end Pos
+
+	for i := 0; i < nl; i++ {
+		scanner.Scan()
+		line := []rune(scanner.Text())
+		grid[i] = line
+		for j, elem := range line {
+			if elem == 'I' {
+				start = Pos{i, j}
+			} else if elem == 'F' {
+				end = Pos{i, j}
+			}
+		}
+	}
+
+	visitado := make([][]bool, nl)
+	for i := range visitado {
+		visitado[i] = make([]bool, nc)
+	}
+
+	cam := NewStack[Pos]()
+	pass := NewStack[Pos]()
+	cam.Push(start)
+
+	for !cam.IsEmpty() {
+		atual := cam.Top()
+		visitado[atual.l][atual.c] = true
+
+		if atual == end {
+			break
+		}
+
+		viz := []Pos{
+			{atual.l, atual.c - 1},
+			{atual.l - 1, atual.c},
+			{atual.l, atual.c + 1},
+			{atual.l + 1, atual.c},
+		}
+
+		var prox *Pos
+		for _, v := range viz {
+			if v.l >= 0 && v.l < nl && v.c >= 0 && v.c < nc && grid[v.l][v.c] != '#' && !visitado[v.l][v.c] {
+				prox = &v
+				break
+			}
+		}
+
+		if prox != nil {
+			cam.Push(*prox)
+		} else {
+			pass.Push(cam.Pop())
+		}
+	}
+	for !cam.IsEmpty() {
+		pop := cam.Pop()
+		grid[pop.l][pop.c] = '.'
+
+		if grid[pop.l][pop.c] != 'I' && grid[pop.l][pop.c] != 'F' {
+			grid[pop.l][pop.c] = '.'
+		}
+	}
+
+	for _, print := range grid {
+		fmt.Println(string(print))
+	}
+}
--- ed/subset_sum/src/go/draft.go
+++ ed/subset_sum/src/go/draft.go
@@ -1,5 +1,40 @@
 package main
+
 import "fmt"
+
+func subset_sum(conjunto []int, k int, tam int, soma int) bool {
+	if soma == k {
+		return true
+	}
+
+	if tam >= len(conjunto) || soma > k {
+		return false
+	}
+
+	if subset_sum(conjunto, k, tam+1, soma+conjunto[tam]) {
+		return true
+	}
+
+    if subset_sum(conjunto, k, tam+1, soma) {
+		return true
+	}
+
+	return false
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	n, k := 0, 0
+	fmt.Scan(&n, &k)
+
+	conjunto := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&conjunto[i])
+	}
+
+	if subset_sum(conjunto, k, 0, 0) {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+
 }
