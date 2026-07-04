46221230bd9f69ae9eff2a21903cd9f708ee464e
Cecília Barbosa <aceciliasb07@gmail.com>
2026-07-04 06:55:25 +0000

    cercado

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -6,9 +6,42 @@ import (
 	"os"
 )
 
+func dfs(grid [][]byte, i int, j int) {
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] != 'O' {
+		return
+	}
+
+	grid[i][j] = '0' // precisa tornar invalido
+
+	dfs(grid, i+1, j)
+	dfs(grid, i-1, j)
+	dfs(grid, i, j+1)
+	dfs(grid, i, j-1)
+}
+
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	l, c := len(board), len(board[0])
+
+	for i := 0; i < l; i++ {
+		dfs(board, i, 0)
+		dfs(board, i, c-1)
+	}
+
+	for i := 0; i < c; i++ {
+		dfs(board, 0, i)
+		dfs(board, l-1, i)
+	}
+
+	for i := 0; i < l; i++ {
+		for j := 0; j < c; j++ {
+			if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			} else if board[i][j] == '0' {
+				board[i][j] = 'O'
+			}
+		}
+	}
 }
 
 // NÃO ALTERE A MAIN
--- ed/ilhas/src/go/main.go
+++ ed/ilhas/src/go/main.go
@@ -6,28 +6,24 @@ import (
 	"os"
 )
 
-func dfs(grid [][]byte, i int, j int) { // quase a msm coisa que o codigo da queimada
-	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] == '0' {
+func dfs(grid [][]byte, l, c int) {
+	if l < 0 || l >= len(grid) || c < 0 || c >= len(grid[0]) || grid[l][c] == '0' {
 		return
 	}
 
-	grid[i][j] = '0' // precisa transfromar em invalido dps
+	grid[l][c] = '0'
 
-	dfs(grid, i+1, j)
-	dfs(grid, i-1, j)
-	dfs(grid, i, j+1)
-	dfs(grid, i, j-1)
-
+	dfs(grid, l+1, c)
+	dfs(grid, l-1, c)
+	dfs(grid, l, c+1)
+	dfs(grid, l, c-1)
 }
 
 // Não modifique a assinatura da função numIslands
 // Ela é a função que será chamada no LeetCode para resolver o problema
 func numIslands(grid [][]byte) int {
-	if len(grid) == 0 || len(grid[0]) == 0 {
-		return 0
-	}
-
 	cont := 0
+
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
 			if grid[i][j] == '1' {
