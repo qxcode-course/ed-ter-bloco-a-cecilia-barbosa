16e194047259aea68e012f5f8b24954b93e9d012
Cecília Barbosa <aceciliasb07@gmail.com>
2026-07-05 05:03:27 +0000

    bfs

--- ed/detonate/src/go/draft.go
+++ ed/detonate/src/go/draft.go
@@ -1,7 +1,60 @@
 package main
+
 import "fmt"
 
+func bfs(graph [][]int, i int, visitado []bool) int {
+	visitado[i] = true
+	cont := 1
+	for _, j := range graph[i] {
+		if !visitado[j] {
+			cont += bfs(graph, j, visitado)
+		}
+	}
+	return cont
+}
+
+func detonate(bombas [][]int) int {
+	n := len(bombas)
+	graph := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		xi, yi, ri := bombas[i][0], bombas[i][1], bombas[i][2]
+		for j := 0; j < n; j++ {
+			if i == j {
+				continue
+			}
+
+			x2, y2 := bombas[j][0], bombas[j][1]
+			dx := xi - x2
+			dy := yi - y2
+			if dx*dx+dy*dy <= ri*ri {
+				graph[i] = append(graph[i], j)
+			}
+		}
+	}
+	result := 0
+	for i := 0; i < n; i++ {
+		visitado := make([]bool, n)
+		cont := bfs(graph, i, visitado)
+		if cont > result {
+			result = cont
+		}
+	}
+	return result
+}
+
 func main() {
+	n, m := 0, 0
 
+	fmt.Scan(&n, &m)
 
+	bombas := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		var xi, yi, ri int
+		fmt.Scan(&xi, &yi, &ri)
+		bombas[i] = []int{xi, yi, ri}
+	}
+
+	fmt.Println(detonate(bombas))
 }
--- ed/rotten/src/go/draft.go
+++ ed/rotten/src/go/draft.go
@@ -1,5 +1,68 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
+func bfs() {}
+
+func orangesRotting(grid [][]int) int {
+	l, c := len(grid), len(grid[0])
+	queue := [][]int{}
+	frescas := 0
+
+	for lin := 0; lin < l; lin++ {
+		for col := 0; col < c; col++ {
+			if grid[lin][col] == 1 {
+				frescas++
+			}
+			if grid[lin][col] == 2 {
+				queue = append(queue, []int{lin, col})
+			}
+		}
+	}
+
+	direcoes := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+	tempo := 0
+
+	for len(queue) > 0 && frescas > 0 {
+		tam := len(queue)
+
+		for i := 0; i < tam; i++ {
+			atual := queue[0]
+			queue = queue[1:]
+			lin, col := atual[0], atual[1]
+			for _, dir := range direcoes {
+				ln, cl := lin+dir[0], col+dir[1]
+				if ln < 0 || ln == l || cl < 0 || cl == c || grid[ln][cl] != 1 {
+					continue
+				}
+				grid[ln][cl] = 2
+				queue = append(queue, []int{ln, cl})
+				frescas--
+			}
+		}
+		tempo++
+	}
+
+	if frescas == 0 {
+		return tempo
+	}
+	return -1
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	l, c := 0, 0
+	fmt.Scan(&l, &c)
+
+	grid := make([][]int, l)
+	for i := 0; i < l; i++ {
+		grid[i] = make([]int, c)
+		for j := 0; j < c; j++ {
+			fmt.Scan(&grid[i][j])
+		}
+	}
+
+	resultado := orangesRotting(grid)
+	fmt.Println(resultado)
 }
